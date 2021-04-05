1bad8b22e06d4dfddb3502225e5ff707d556064e
aculkin <andrewsculkin@gmail.com>
2021-04-05 16:13:44 -0400

    Add the dashboard page

---
+++ pages/dashboard/index.js
@@ -0,0 +1,12 @@
+import { NavbarLayout } from '../../layouts/NavbarLayout'
+import { Dashboard } from '../../components/page-components/Dashboard'
+
+export const DashboardPage = () => {
+  return (
+    <NavbarLayout>
+      <Dashboard />
+    </NavbarLayout>
+  )
+}
+
+export default DashboardPage
