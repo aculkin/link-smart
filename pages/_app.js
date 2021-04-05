a56c247bf29273e32d9a3724479879a6b8f8d7a4
aculkin <andrewsculkin@gmail.com>
2021-04-05 16:04:49 -0400

    Add redux provider to App.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import 'semantic-ui-css/semantic.min.css'
+import 'toastify-js/src/toastify.css'
+
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { Provider } from 'react-redux'
+import { useStore } from '../redux'
+
+const App = ({ Component, pageProps }) => {
+  const store = useStore(pageProps.initialReduxState)
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
-export default MyApp
+export default App
