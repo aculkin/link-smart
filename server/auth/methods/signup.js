cf6ef8c0d8b764de6e95259ccbd1d33f246b7133
aculkin <andrewsculkin@gmail.com>
2021-04-06 14:35:54 -0400

    Add initial account creation to the signup route and front end

--- server/auth/methods/signup.js
+++ server/auth/methods/signup.js
@@ -1,17 +1,26 @@
-const { User } = require('../../database/models')
+const { User, Account, User_Account } = require('../../database/models')
 
 const signup = async (req, res, next) => {
   try {
-    const { email, firstName, lastName, password } = req.body
-    console.log(req.body)
+    const { email, firstName, lastName, password, accountSlug } = req.body
     const user = await User.create({
       email,
       password,
       firstName,
       lastName,
     })
+    let account
+    if (accountSlug) {
+      account = await Account.create({ slug: accountSlug })
+      await User_Account.create({
+        admin: true,
+        userId: user.id,
+        accountId: account.id,
+        name: accountSlug,
+      })
+    }
     req.login(user, (error) => {
-      error ? next(error) : res.json(user)
+      error ? next(error) : res.json({ user, account })
     })
   } catch (error) {
     next(error)
