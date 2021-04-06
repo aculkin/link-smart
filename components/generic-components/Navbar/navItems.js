ef533c2b71e484463b14ba049b581ed321730712
aculkin <andrewsculkin@gmail.com>
2021-04-06 14:33:24 -0400

    Add seperation in the nav bar for when someone is logged in vs. logged out

--- components/generic-components/Navbar/navItems.js
+++ components/generic-components/Navbar/navItems.js
@@ -1,6 +1,11 @@
-export const navItems = [
+export const notLoggedInNavItems = [
   { name: 'Home', address: '/' },
-  { name: 'Dashboard', address: '/dashboard' },
+  { name: 'About', address: '/about' },
+  // { name: 'Dashboard', address: '/dashboard' },
 ]
 
-export default navItems
+export const loggedInNavItems = [
+  { name: 'Home', address: '/' },
+  { name: 'About', address: '/about' },
+  { name: 'Dashboard', address: '/dashboard' },
+]
