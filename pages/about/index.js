f333c6c92b1adaab93d66f6efb87d1048e248b57
aculkin <andrewsculkin@gmail.com>
2021-04-06 15:05:16 -0400

    Add placeholder about page

---
+++ pages/about/index.js
@@ -0,0 +1,12 @@
+import { About } from '../../components/page-components/About'
+import { NavbarLayout } from '../../layouts'
+
+export const AboutPage = () => {
+  return (
+    <NavbarLayout>
+      <About />
+    </NavbarLayout>
+  )
+}
+
+export default AboutPage
