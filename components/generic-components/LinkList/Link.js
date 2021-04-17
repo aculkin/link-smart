051b5e9ad0654c04309ce7230eb3be4c87b3387d
Andrew Culkin <andrewsculkin@gmail.com>
2021-04-17 15:24:21 -0400

    Add priority formating for link buttons

--- components/generic-components/LinkList/Link.js
+++ components/generic-components/LinkList/Link.js
@@ -1,9 +1,15 @@
 import { Button } from 'semantic-ui-react'
 
 export const Link = ({ link }) => {
-  const { id, name, url } = link
+  const { id, name, url, priority } = link
   return (
-    <Button href={url} fluid size="large" style={{ margin: '5px' }}>
+    <Button
+      color={priority ? 'blue' : null}
+      href={url}
+      fluid
+      size="large"
+      style={{ margin: '5px' }}
+    >
       {name}
     </Button>
   )
