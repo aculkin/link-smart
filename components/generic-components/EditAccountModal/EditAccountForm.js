311dc3ebaaba0a5c47e066661c735bcc0674c916
Andrew Culkin <andrewsculkin@gmail.com>
2021-04-17 12:21:36 -0400

    Add edit account component

---
+++ components/generic-components/EditAccountModal/EditAccountForm.js
@@ -0,0 +1,93 @@
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { Form, Input, Divider } from 'semantic-ui-react'
+
+import { removeAccountThunk, editAccountThunk } from '../../../redux/accounts'
+import { toast } from '../../../utility/front-end'
+
+export const EditAccountForm = ({ account, setOpen }) => {
+  const dispatch = useDispatch()
+  const [loading, setLoading] = useState(false)
+  const [deleteLoading, setDeleteLoading] = useState(false)
+  const [editedAccount, setEditedAccount] = useState(account)
+
+  const handleChange = (_e, { name, value }) => {
+    setEditedAccount({ ...editedAccount, [name]: value })
+  }
+
+  const handleSubmit = () => {
+    setLoading(true)
+    dispatch(
+      editAccountThunk({
+        accountId: account?.id,
+        accountDetails: editedAccount,
+        afterSuccess: () => {
+          setLoading(false)
+        //   setOpen(false)
+        },
+        afterFailure: () => {
+          setLoading(false)
+          toast('Something went wrong', 'negative')
+        },
+      })
+    )
+    console.log('submitting')
+  }
+
+  const handleDeleteAccount = () => {
+    setDeleteLoading(true)
+    dispatch(
+      removeAccountThunk({
+        accountId: account?.id,
+        afterSuccess: () => {
+          setDeleteLoading(false)
+          setOpen(false)
+        },
+        afterFailure: () => {
+          setDeleteLoading(false)
+          toast('Something went wrong', 'negative')
+        },
+      })
+    )
+  }
+
+  return (
+    <Form>
+      <Input
+        fluid
+        name="slug"
+        label="https://linksmart.app/"
+        onChange={handleChange}
+        value={editedAccount.slug}
+      />
+      <Divider hidden />
+      <Form.Input
+        fluid
+        name="name"
+        label="Account Name"
+        onChange={handleChange}
+        value={editedAccount.name}
+      />
+      <Form.Group>
+        <Form.Button
+          loading={deleteLoading}
+          color="red"
+          onClick={handleDeleteAccount}
+        >
+          Delete this account
+        </Form.Button>
+        <Form.Button
+          loading={loading}
+          floated="right"
+          color="green"
+          type="submit"
+          onClick={handleSubmit}
+        >
+          Save Changes
+        </Form.Button>
+      </Form.Group>
+    </Form>
+  )
+}
+
+export default EditAccountForm
---
+++ components/generic-components/EditAccountModal/index.js
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import { Modal, Button, Header, Icon } from 'semantic-ui-react'
+
+import { EditAccountForm } from './EditAccountForm'
+
+export const EditAccountModal = ({ account }) => {
+  const [open, setOpen] = useState(false)
+  const setModalState = (state) => () => setOpen(state)
+  return (
+    <>
+      <Button fluid icon labelPosition="right" onClick={setModalState(true)}>
+        <Icon name="settings" />
+        Account Settings
+      </Button>
+      <Modal
+        closeIcon
+        open={open}
+        onClose={setModalState(false)}
+        onOpen={setModalState(true)}
+      >
+        <Header content={`Settings for page: ${account?.name}`} />
+        <Modal.Content>
+          <EditAccountForm account={account} setOpen={setOpen} />
+        </Modal.Content>
+      </Modal>
+    </>
+  )
+}
+
+export default EditAccountModal
