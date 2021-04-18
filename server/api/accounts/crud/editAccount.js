7a7df65c118cfb8e3bd4532ebc398c25a9ac2e1d
Andrew Culkin <andrewsculkin@gmail.com>
2021-04-18 09:41:56 -0400

    Add check for existing slug when creating or editing an account on backend

--- server/api/accounts/crud/editAccount.js
+++ server/api/accounts/crud/editAccount.js
@@ -1,12 +1,21 @@
 const { Account } = require('../../../database/models')
+const { Op } = require('sequelize')
 
 const editAccount = async (req, res, next) => {
   try {
     const { id } = req.params
     const { name, slug } = req.body
-    const account = await Account.findByPk(id)
-    const updatedAccount = await account.update({ name, slug })
-    res.status(200).send(updatedAccount)
+    const existingAccount = await Account.findOne({
+      where: { slug, id: { [Op.ne]: id } },
+    })
+    if (existingAccount) {
+      const account = await Account.findByPk(id)
+      res.status(205).json(account)
+    } else {
+      const account = await Account.findByPk(id)
+      const updatedAccount = await account.update({ name, slug })
+      res.status(200).json(updatedAccount)
+    }
   } catch (error) {
     next(error)
   }
