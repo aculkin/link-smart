06d481eb78bf932fdc80eddd70705c0bf3fbac22
Andrew Culkin <andrewsculkin@gmail.com>
2021-04-24 10:26:30 -0400

    Add  register view to the link page

--- components/page-components/Link/index.js
+++ components/page-components/Link/index.js
@@ -1,10 +1,25 @@
+import { useEffect } from 'react'
 import Head from 'next/head'
 import { Header, Container, Image, Divider } from 'semantic-ui-react'
 import { LinkList } from '../../generic-components/LinkList'
+import { API } from '../../../API'
 
-export const Link = ({ account }) => {
+export const Link = ({ account, preview }) => {
+  const registerView = async () => {
+    try {
+      const viewDetails = {}
+      if (process.browser && !preview) {
+        await API.views.recordView(account.id, viewDetails)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(registerView, [])
+
   return (
-    <Container text textAlign="center">
+    <Container fluid text textAlign="center">
       <Head>
         <title>{account?.name}</title>
         <meta name="description" content={'bio about myself'} />
@@ -21,7 +36,12 @@ export const Link = ({ account }) => {
         {account?.name}
         <Header.Subheader>@{account?.slug}</Header.Subheader>
       </Header>
-      <LinkList links={account?.links || []} />
+      <LinkList
+        links={account?.links || []}
+        preview={preview}
+        priorityColor={account?.priorityColor}
+        linkColor={account?.linkColor}
+      />
     </Container>
   )
 }
