ee94f5e3eb6042cd188cfa36afb5eb91c8832f24
Andrew Culkin <andrewsculkin@gmail.com>
2021-04-24 10:27:46 -0400

    Add iphone and color picker to dashboard

--- components/page-components/Dashboard/index.js
+++ components/page-components/Dashboard/index.js
@@ -17,6 +17,9 @@ import { DashboardLink } from '../../generic-components/DashboardLink'
 import { NewLinkButton } from '../../generic-components/NewLinkButton'
 import { EditAccountModal } from '../../generic-components/EditAccountModal'
 import { NewAccountButton } from '../../generic-components/NewAccountButton'
+import { IphoneContainer } from '../../generic-components/IphoneContainer'
+import { ColorSelector } from '../../generic-components/ColorSelector'
+import { Link } from '../../page-components/Link'
 import { formatSUIOptions } from '../../../utility/front-end'
 
 export const Dashboard = () => {
@@ -46,7 +49,7 @@ export const Dashboard = () => {
       <Header textAlign="center" as="h1">
         Dashboard: {selectedAccount?.name}
       </Header>
-      <Grid>
+      <Grid stackable>
         <Grid.Row>
           <Grid.Column width="4">
             <NewAccountButton
@@ -112,7 +115,22 @@ export const Dashboard = () => {
               </Segment>
             )}
           </Grid.Column>
-          <Grid.Column width="8"></Grid.Column>
+          <Grid.Column width="4">
+            <Segment>
+              <ColorSelector account={selectedAccount} />
+            </Segment>
+          </Grid.Column>
+          <Grid.Column width="4">
+            <IphoneContainer>
+              <Link
+                account={{
+                  ...selectedAccount,
+                  links: links.filter((link) => link?.active && link?.url),
+                }}
+                preview={true}
+              />
+            </IphoneContainer>
+          </Grid.Column>
         </Grid.Row>
       </Grid>
       <Divider />
