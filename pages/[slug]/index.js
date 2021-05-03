cafc22d469528c2c56d33c2fc16d5e54232239ed
Andrew Culkin <andrewsculkin@gmail.com>
2021-05-03 18:04:48 -0400

    Add backgroundColor render capability to link page component

--- pages/[slug]/index.js
+++ pages/[slug]/index.js
@@ -6,7 +6,7 @@ import { getAccountWithLinks } from '../../next-data-fetching'
 export const DynamicLinkPage = ({ account }) => {
   const router = useRouter()
   const passthroughLink = account?.links?.find((link) => link?.passthrough)
-  console.log(account)
+  
   if (passthroughLink?.url && process.browser) {
     router.replace(passthroughLink?.url)
     return <div>Redirecting...</div>
