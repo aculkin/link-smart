887a5dde9fdb6cf96d3861195e637c899580dbd3
Andrew Culkin <andrewsculkin@gmail.com>
2021-05-03 18:04:48 -0400

    Add backgroundColor render capability to link page component

--- components/page-components/Link/index.js
+++ components/page-components/Link/index.js
@@ -1,8 +1,18 @@
 import { useEffect } from 'react'
 import Head from 'next/head'
-import { Header, Container, Image, Divider } from 'semantic-ui-react'
+import { Header, Container, Image } from 'semantic-ui-react'
 import { LinkList } from '../../generic-components/LinkList'
 import { API } from '../../../API'
+
+const getBackgroundStyle = (backgroundColor) => {
+  if (backgroundColor?.length === 7) {
+    return { minHeight: '100vh', backgroundColor }
+  } else if (backgroundColor?.length > 7) {
+    return { minHeight: '100vh', backgroundImage: backgroundColor }
+  } else {
+    return { minHeight: '100vh' }
+  }
+}
 
 export const Link = ({ account, preview }) => {
   const registerView = async () => {
@@ -19,30 +29,31 @@ export const Link = ({ account, preview }) => {
   useEffect(registerView, [])
 
   return (
-    <Container fluid text textAlign="center">
-      <Head>
-        <title>{account?.name}</title>
-        <meta name="description" content={'bio about myself'} />
-      </Head>
-      <Divider hidden />
-      <Image
-        centered
-        alt={account?.image?.altText}
-        src={account?.image?.url}
-        size="tiny"
-        circular
-      />
-      <Header>
-        {account?.name}
-        <Header.Subheader>@{account?.slug}</Header.Subheader>
-      </Header>
-      <LinkList
-        links={account?.links || []}
-        preview={preview}
-        priorityColor={account?.priorityColor}
-        linkColor={account?.linkColor}
-      />
-    </Container>
+    <div style={getBackgroundStyle(account?.backgroundColor)}>
+      <Container fluid text textAlign="center">
+        <Head>
+          <title>{account?.name}</title>
+          <meta name="description" content={'bio about myself'} />
+        </Head>
+        <Image
+          centered
+          alt={account?.image?.altText}
+          src={account?.image?.url}
+          size="tiny"
+          circular
+        />
+        <Header>
+          {account?.name}
+          <Header.Subheader>@{account?.slug}</Header.Subheader>
+        </Header>
+        <LinkList
+          links={account?.links || []}
+          preview={preview}
+          priorityColor={account?.priorityColor}
+          linkColor={account?.linkColor}
+        />
+      </Container>
+    </div>
   )
 }
 
