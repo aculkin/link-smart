c2810627e8540f2692d2c8b767c2873138c4b3f6
Andrew Culkin <andrewsculkin@gmail.com>
2021-05-04 10:57:49 -0400

    Include Clicks when loading links and Views when loading the account

--- server/api/accounts/other/loadMyAccounts.js
+++ server/api/accounts/other/loadMyAccounts.js
@@ -1,8 +1,10 @@
-const { User, Account } = require('../../../database/models')
+const { User, Account, View } = require('../../../database/models')
 
 const loadMyAccounts = async (req, res, next) => {
   try {
-    const accounts = await req?.user?.getAccounts()
+    const accounts = await req?.user?.getAccounts({
+      include: [{ model: View }],
+    })
     res.status(200).json(accounts || null)
   } catch (error) {
     next(error)
--- server/api/links/other/getAccountLinks.js
+++ server/api/links/other/getAccountLinks.js
@@ -1,9 +1,9 @@
-const { Link } = require('../../../database/models')
+const { Link, Click } = require('../../../database/models')
 
 const getAccountLinks = async (req, res, next) => {
   try {
     const { accountId } = req.params
-    const links = await Link.findAll({ where: { accountId } })
+    const links = await Link.findAll({ where: { accountId }, include: Click })
     res.status(200).json(links)
   } catch (error) {
     next(error)
