b5dc74998056218a9fc7e59dbccbe7af920c2b9e
Andrew Culkin <andrewsculkin@gmail.com>
2021-05-04 11:01:01 -0400

    Add "validation" for url beginning with http or https

--- components/generic-components/LinkList/Link.js
+++ components/generic-components/LinkList/Link.js
@@ -2,11 +2,21 @@ import { useRouter } from 'next/router'
 import { Button } from 'semantic-ui-react'
 import { API } from '../../../API'
 
+const convertUrl = (uneditedUrl) => {
+  const url = uneditedUrl.toLowerCase()
+  if (url.startsWith('http://') || url.startsWith('https://')) {
+    return url
+  } else {
+    return `https://${url}`
+  }
+}
+
 export const Link = ({ link, preview, priorityColor, linkColor }) => {
   const router = useRouter()
   const { id: linkId, name, url, priority } = link
 
   const navigateToUrl = async () => {
+    const correctUrl = convertUrl(url)
     try {
       const clickDetails = {}
       if (process.browser && !preview) {
@@ -15,7 +25,7 @@ export const Link = ({ link, preview, priorityColor, linkColor }) => {
     } catch (error) {
       console.log(error)
     }
-    router.push(url)
+    router.push(correctUrl)
   }
 
   return (
