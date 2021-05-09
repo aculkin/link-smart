2f033a42b25d96c6c3e36465c19cbe12801f641e
Andrew Culkin <andrewsculkin@gmail.com>
2021-05-09 09:33:58 -0400

    Add other dashboard components

---
+++ components/page-components/Dashboard/Settings.js
@@ -0,0 +1,7 @@
+import { EditAccountModal } from '../../generic-components/EditAccountModal'
+
+export const Settings = ({ selectedAccount }) => {
+  return <EditAccountModal account={selectedAccount} />
+}
+
+export default Settings
