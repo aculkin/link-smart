06bd219a0a68f3ae22cc1a753ef0a6aba60a1907
Andrew Culkin <andrewsculkin@gmail.com>
2021-05-09 09:33:32 -0400

    Incorporate changes to make the LinkPage component only expand to 100% but show in the whole page for the actual one

--- pages/[slug]/index.js
+++ pages/[slug]/index.js
@@ -6,12 +6,16 @@ import { getAccountWithLinks } from '../../next-data-fetching'
 export const DynamicLinkPage = ({ account }) => {
   const router = useRouter()
   const passthroughLink = account?.links?.find((link) => link?.passthrough)
-  
+
   if (passthroughLink?.url && process.browser) {
     router.replace(passthroughLink?.url)
     return <div>Redirecting...</div>
   } else {
-    return <Link account={account} />
+    return (
+      <div style={{ height: '100vh', width: '100vw' }}>
+        <Link account={account} />
+      </div>
+    )
   }
 }
 
